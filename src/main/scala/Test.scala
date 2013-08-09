65c514bd85b3d421511b38e528b1925e46b9a93f
cyril <cyril_jayet@yahoo.fr>
2013-08-09 19:38:47 +0200

    Commit 08/08

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,108 +1,52 @@
-/*import fr.geocite.marius._
-import fr.geocite.marius.one.zero._
+/**
+ * Created with IntelliJ IDEA.
+ * User: paris
+ * Date: 07/08/13
+ * Time: 15:27
+ * To change this template use File | Settings | File Templates.
+ */
 import fr.geocite.simpuzzle._
-import spray.json._
-import org.apache.commons.math3.stat.StatUtils
-
-import scala.util.Random
-
-case class XY_JSON(key: String, values: Seq[Map[String, Double]])
-
-object MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val XYFormat = jsonFormat2(XY_JSON)
-}
-
-object JSONConverter {
-  def main(args: Array[String]) = {
-
-    import rng._
-
-    val m = new StepByStep with MariusInitialState with MariusStep with TimeEndingCondition with MariusFile {
-      def hydrocarbonBonus: Double = 0.01
-
-      def rate: Double = 0.01
-
-      def stdRate: Double = 0.005
-
-      def maxStep: Int = 100
-    }
-
-
-    println(m.states.toList.last.cities.map(_.population).sorted.reverse)
-
-    println(StatUtils.mean(Array(5.0,8.0,9.0,8.0,4.0,5.0,2.0,4.0)))
-
-    val XYZTable = List(Array(1, 1.8),Array(2, 8.8),Array(3, 1.0))
-
-    val xyzMappedTable = XYZTable.map { v ⇒
-      Map[String, Double]("id" -> v(0), "pop2010" -> v(1))
-    }.toIndexedSeq
-
-    val cities = List("1")
-
-    import MyJsonProtocol._
-
-    val JsonData = cities.map{ case (ci) => XY_JSON(ci, xyzMappedTable)}.toJson
-
-    println(JsonData.toString())
-
-  }
-}
-
-object Test extends App {
-
-  import rng._
-
-  val m = new StepByStep with MariusInitialState with MariusStep with TimeEndingCondition with MariusFile {
-    def hydrocarbonBonus: Double = 0.01
-
-    def rate: Double = 0.01
-
-    def stdRate: Double = 0.005
-
-    def maxStep: Int = 100
-  }
-
-
-  println(m.states.toList.last.cities.map(_.population).sorted.reverse)
-}*/
-
-import fr.geocite.simpuzzle._
-import fr.geocite.marius.one.zero._
 import fr.geocite.marius._
+import fr.geocite.marius.zero.one._
 import util.Random
+import scala.util.Try
 
-object Test extends App {
-implicit val rng = new Random(42)
+object Test {
 
-val marius = new StepByStep with MariusInitialState with MariusStep with TimeEndingCondition with MariusFile with PowerInitialWealth {
-// Members declared in fr.geocite.marius.one.zero.MariusStep
-def adjustConsumption: Double = 0.6
-def adjustProductivity: Double = 0.3
-def capitalShareOfTaxes: Double = 0.0
-def conversionFactor: Double = 50.0
-def distanceDecay: Double = 1.0
-def distanceOrderBuy: Double = 0.6
-def distanceOrderSell: Double = 0.4
-def partnerMultiplier: Double = 8.0
-def territorialTaxes: Double = 0.4
+  implicit val rng = new Random(42)
 
-// Members declared in fr.geocite.marius.one.zero.PowerInitialWealth
-def wealthExponent: Double = 1.1
+  val marius = new StepByStep with MariusInitialState with MariusStep with TimeEndingCondition with MariusFile {
+    def hydrocarbonBonus: Double = 0.01
+    def rate: Double = 0.01
+    def stdRate: Double = 0.005
+    def maxStep: Int = 10
+  }
 
-// Members declared in fr.geocite.simpuzzle.TimeEndingCondition
-def maxStep: Int = 51
+  def takeOneStep(n:Int):Seq[marius.City] = {
+    if (marius.maxStep>n) {
+      val l=marius.states.drop(n-1).next.cities
+      l
+    }
+    else List()
+  }
+
+  def takeOneCity(id:String) = {
+    val it=marius.states
+    val n=id.length
+    var res=it.next.cities.filter(e=>e.okato.take(n)==id)
+    while (it.hasNext) {
+      val l=it.next.cities.filter(e=>e.okato.take(n)==id)
+      res=res++l
+    }
+    res
+  }
+
+  def takeNLoopBis(n:Int,l:Seq[marius.City],):Seq[marius.City] = n match {
+    case 0 => l
+    case _ => l++takeNLoopBis(n-1,takeOneStep(marius.maxStep-1))
+  }
+
+  def takeNLoop(n:Int)=takeNLoopBis(n,List())
+
+
 }
-
-marius.states.foreach(s => println(s.step))
-
-import scalax.io._
-val output:Output = Resource.fromFile("/tmp/marius.csv")
-
-marius.states.foreach {
-s => println(s.step)
-s.cities.zipWithIndex.foreach{
-case(c, id) => output.write(s"${s.step},$id,${c.population},${c.wealth},${c.region},${c.capital},27.0\n")
-}
-}}
-
